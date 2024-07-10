03e55540f0f553951f12599c87ff315fac1d5917
einherer <johanneshumann@hotmail.com>
2024-07-10 13:59:30 +0200

    feat: shorten views, and url interface

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListCreate, PostDetail, PostUpdate, PostDelete, CommentCreate, CommentDetail, CommentUpdate, CommentDelete
+from .views import PostListCreate, PostDetail, CommentCreate, CommentDetail
 
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -7,11 +7,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("posts/", PostListCreate.as_view(), name="posts"),
     path("posts/<int:pk>/", PostDetail.as_view(), name="post"),
-    path("posts/<int:pk>/update/", PostUpdate.as_view(), name="post_update"),
-    path("posts/<int:pk>/delete/", PostDelete.as_view(), name="post_delete"),
     path("comments/", CommentCreate.as_view(), name="comments"),
     path("comments/<int:pk>/", CommentDetail.as_view(), name="comment"),
-    path("comments/<int:pk>/update/", CommentUpdate.as_view(), name="comment_update"),
-    path("comments/<int:pk>/delete/", CommentDelete.as_view(), name="comment_delete"),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,64 +1,32 @@
 from rest_framework import generics, permissions
 from .models import Post, Comment
 from .serializer import PostSerializer, CommentSerializer
+from .permission import IsOwnerOrReadOnly
 
 # Create your views here.
 class PostListCreate(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
         
-class PostDetail(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    
-    
-    
-class PostUpdate(generics.UpdateAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    
-    def perform_update(self, serializer):
-        serializer.save(author=self.request.user)
-        
-class PostDelete(generics.DestroyAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    
-    def perform_destroy(self, instance):
-        return super().perform_destroy(instance)
 
-class CommentCreate(generics.CreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class CommentCreate(generics.ListCreateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
         
-class CommentDetail(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     
-class CommentUpdate(generics.UpdateAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    
-    def perform_update(self, serializer):
-        serializer.save(author=self.request.user)
-        
-class CommentDelete(generics.DestroyAPIView):  
-    permission_classes = [permissions.IsAuthenticated]
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    
-    def perform_destroy(self, instance):
-        return super().perform_destroy(instance)
